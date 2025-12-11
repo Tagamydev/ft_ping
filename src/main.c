f156d2159c0d88fe63b32a09d73e18a67cb6e96e
samusanc <samu2004ssmm@gmail.com>
2025-12-11 15:09:44 +0100

    fix(ping print)

--- src/main.c
+++ src/main.c
@@ -106,7 +106,7 @@ int	ft_ping(t_node *new_ipv4)
 	if (ping->flags.v)
 	{
 		uint16_t id = getpid() & 0xFFFF;
-		printf(", id = 0x%04x = %d\n", id, id);
+		printf(", id = 0x%04x = %d", id, id);
 	}
 	pintf("\n");
 	//printf("number:%d, flag:%d\n", ping->flags.number, ping->flags.c);
